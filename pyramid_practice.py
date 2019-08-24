f49805c279f52ab7ecaccaf2fdee4522417e80b5
tahmid-sadi <tahbaquee@gmail.com>
2019-08-24 20:32:13 +0600

    3rd testing GIT operations.

---
+++ pyramid_practice.py
@@ -0,0 +1,15 @@
+height = int(input('Enter height = '))
+for r in range(1, height + 1):
+    for c in range(r):
+        print('*', end=' ')
+    print(end='\n')
+hl = height - 1
+for lr in range(hl):
+    count = hl
+    while hl > 0:
+        print('*', end=' ')
+        hl -= 1
+    print(end='\n')
+    hl = count - 1
+
+
