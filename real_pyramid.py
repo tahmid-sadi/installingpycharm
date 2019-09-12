8a054832090f52d051d2dca91b7b32d68c22317e
tahmid-sadi <tahbaquee@gmail.com>
2019-09-12 20:51:47 +0600

    Checking my knowledge on Git

---
+++ real_pyramid.py
@@ -0,0 +1,12 @@
+x = int(input('Enter height = '))
+for i in range(1, x + 1):
+    print('*' * i)
+h = x - 1
+while h > 0:
+    print('*' * h)
+    h -= 1
+
+for j in range(1, x + 1):
+    for k in range(1, j+1):
+        print('*', end=' ')
+    print(end='\n')
