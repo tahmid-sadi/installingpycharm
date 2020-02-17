83b82ebedd8ee58b961aafcee5910000f325551a
tahmid-sadi <tahbaquee@gmail.com>
2020-02-17 11:07:55 +0600

    Learning class and checking.

--- class.py
+++ class.py
@@ -8,6 +8,12 @@ class Test:
         for i in range(number):
             self.a.append(i*i)
 
+    def find_even(self):
+        for i in range(n):
+            if self.a[i] % 2 == 0:
+                print(self.a[i])
+
 
 Check = Test(n)
 print(Check.a)
+Check.find_even()
